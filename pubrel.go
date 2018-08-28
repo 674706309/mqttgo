36d3e46f5d5a27d8507adcdba1168a2233383e6f
FoxCATT <whgg1284@gmail.com>
2018-08-28 17:53:38 +0800

    mqtt pubrel

--- pubrel.go
+++ pubrel.go
@@ -1,8 +1,53 @@
 package mqtt
 
+import (
+	"encoding/binary"
+	"fmt"
+)
+
 type Pubrel struct {
 	//固定头
-	FixedHeader
-	//可变头
-	PacketIdentifier uint16 //报文标识符
+	Header Header
+}
+
+func NewPubrel() (p *Pubrec) {
+	p.Header.SetType(TYPE_PUBREL)
+	p.Header.SetFlag(TYPE_FLAG_PUBREL)
+	p.Header.SetRemainingLength(2)
+	return
+}
+func (p Pubrel) String() string {
+	return fmt.Sprintf("%s, PacketID=%d", p.Header, p.Header.GetPacketID())
+}
+func (p *Pubrel) GetLength() int {
+	ml, _ := binary.Uvarint(p.Header.GetRemainingLength())
+	return p.Header.getLength() + int(ml)
+}
+func (p *Pubrel) encode(dst []byte) (total int, err error) {
+	var (
+		n int
+	)
+	total = 0
+	n, err = p.Header.encode(dst[total:])
+	total += n
+	if err != nil {
+		return
+	}
+	binary.BigEndian.PutUint16(dst[total:], p.Header.GetPacketID())
+	total += 2
+	return
+}
+func (p *Pubrel) decode(src []byte) (total int, err error) {
+	var (
+		n int
+	)
+	total = 0
+	n, err = p.Header.decode(src[total:])
+	total += n
+	if err != nil {
+		return
+	}
+	p.Header.SetPacketID(binary.BigEndian.Uint16(src[total:]))
+	total += 2
+	return
 }
