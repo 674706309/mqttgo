c3ca3abd28e326a2c8c9987fc768258d92ffa135
FoxCATT <whgg1284@gmail.com>
2018-08-29 10:29:27 +0800

    mqtt suback

--- unsuback.go
+++ unsuback.go
@@ -1,5 +1,53 @@
 package mqtt
 
+import (
+	"encoding/binary"
+	"fmt"
+)
+
 type UnSuback struct {
 	Header Header
+}
+
+func NewUnSuback() (u *UnSuback) {
+	u.Header.SetType(TYPE_SUBACK)
+	u.Header.SetFlag(TYPE_FLAG_SUBACK)
+	return
+}
+func (u UnSuback) String() string {
+	return fmt.Sprintf("%s, PacketID=%d", u.Header, u.Header.GetPacketID())
+}
+func (u *UnSuback) Length() int {
+	return u.Header.Length() + u.GetRemainingLength()
+}
+func (u *UnSuback) GetRemainingLength() int {
+	return 2
+}
+func (u *UnSuback) encode(dst []byte) (total int, err error) {
+	var (
+		n int
+	)
+	total = 0
+	n, err = u.Header.encode(dst[total:])
+	total += n
+	if err != nil {
+		return
+	}
+	binary.BigEndian.PutUint16(dst[total:], s.Header.GetPacketID())
+	total += n
+	return
+}
+func (u *UnSuback) decode(src []byte) (total int, err error) {
+	var (
+		n int
+	)
+	total = 0
+	n, err = u.Header.decode(src[total:])
+	total += n
+	if err != nil {
+		return
+	}
+	u.Header.SetPacketID(binary.BigEndian.Uint16(src[total:]))
+	total += 2
+	return
 }
