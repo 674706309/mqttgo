db555301fb714dd6209c91a5b5fda18ae2e0dffa
FoxCATT <whgg1284@gmail.com>
2018-08-29 10:22:28 +0800

    mqtt disconnect

--- disconnect.go
+++ disconnect.go
@@ -1,6 +1,37 @@
 package mqtt
 
+import "fmt"
+
 type Disconnect struct {
-	//固定头
 	Header Header
+}
+
+func NewDisconnect() (p *Disconnect) {
+	p.Header.SetType(TYPE_DISCONNECT)
+	p.Header.SetFlag(TYPE_FLAG_DISCONNECT)
+	return
+}
+func (p Disconnect) String() string {
+	return fmt.Sprintf("%s", p.Header)
+}
+func (p *Disconnect) Length() int {
+	return p.Header.Length()
+}
+func (p *Disconnect) encode(dst []byte) (total int, err error) {
+	var (
+		n int
+	)
+	total = 0
+	n, err = p.Header.encode(dst[total:])
+	total += n
+	return
+}
+func (p *Disconnect) decode(src []byte) (total int, err error) {
+	var (
+		n int
+	)
+	total = 0
+	n, err = p.Header.decode(src[total:])
+	total += n
+	return
 }
